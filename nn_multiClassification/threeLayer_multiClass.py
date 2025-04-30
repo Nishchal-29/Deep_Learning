89afee9faa9c3746607343a0e260f88b977a4165
Nishchal Singh <nishchalsingh9a@gmail.com>
2025-04-30 19:34:22 +0530

    multiClass file updated with plots

--- nn_multiClassification/threeLayer_multiClass.py
+++ nn_multiClassification/threeLayer_multiClass.py
@@ -4,6 +4,9 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import warnings
 warnings.filterwarnings("ignore")
 
+# import os
+# os.environ["CUDA_VISIBLE_DEVICES"] = "-1" 
+
 # importing required libraries
 import tensorflow as tf
 import numpy as np
@@ -42,5 +45,119 @@ def normalize(image):
 
 new_train = x_train.map(normalize)
 new_test = x_test.map(normalize)
+new_y_train = y_train.map(lambda x: tf.one_hot(x, depth=6))
+new_y_test = y_test.map(lambda x: tf.one_hot(x, depth=6))
 
+# initializing parameters
+def initialize_parameters():
+    initializer = tf.keras.initializers.GlorotNormal(seed=1)
+    W1 = tf.Variable(initializer(shape=(25, 12288)))
+    b1 = tf.Variable(initializer(shape=(25, 1)))
+    W2 = tf.Variable(initializer(shape=(12, 25)))
+    b2 = tf.Variable(initializer(shape=(12, 1)))
+    W3 = tf.Variable(initializer(shape=(6, 12)))
+    b3 = tf.Variable(initializer(shape=(6, 1)))
+    parameters = {
+        "W1": W1,
+        "b1": b1,
+        "W2": W2,
+        "b2": b2,
+        "W3": W3,
+        "b3": b3
+    }
+    return parameters
 
+# forward propagation
+def forward_propagation(X, parameters):
+    W1 = parameters['W1']
+    b1 = parameters['b1']
+    W2 = parameters['W2']
+    b2 = parameters['b2']
+    W3 = parameters['W3']
+    b3 = parameters['b3']
+
+    Z1 = tf.add(tf.matmul(W1, X), b1)
+    A1 = tf.keras.activations.relu(Z1)
+    Z2 = tf.add(tf.matmul(W2, A1), b2)
+    A2 = tf.keras.activations.relu(Z2)
+    Z3 = tf.add(tf.matmul(W3, A2), b3)
+    
+    return Z3
+    
+def total_loss(logits, labels):
+    total_loss = tf.reduce_sum(tf.keras.metrics.categorical_crossentropy(tf.transpose(labels),tf.transpose(logits),from_logits=True))
+    return total_loss
+
+def model(X_train, Y_train, X_test, Y_test, learning_rate=0.0001, num_epochs=1500, minibatch_size=32, print_cost=True):
+    costs = []
+    train_acc = []
+    test_acc = []
+    
+    parameters = initialize_parameters()
+    W1 = parameters['W1']
+    b1 = parameters['b1']
+    W2 = parameters['W2']
+    b2 = parameters['b2']
+    W3 = parameters['W3']
+    b3 = parameters['b3']
+    
+    optimizer = tf.keras.optimizers.Adam(learning_rate)
+    test_accuracy = tf.keras.metrics.CategoricalAccuracy()
+    train_accuracy = tf.keras.metrics.CategoricalAccuracy()
+    dataset = tf.data.Dataset.zip((X_train, Y_train))
+    test_dataset = tf.data.Dataset.zip((X_test, Y_test))
+    
+    m = dataset.cardinality().numpy()
+    minibatches = dataset.batch(minibatch_size).prefetch(8)
+    test_minibatches = test_dataset.batch(minibatch_size).prefetch(8)
+    
+    for epoch in range(num_epochs):
+        epoch_total_loss = 0.
+        train_accuracy.reset_state()
+        for minibatch in minibatches:
+            (minibatch_X, minibatch_Y) = minibatch
+            with tf.GradientTape() as tape:
+                logits = forward_propagation(tf.transpose(minibatch_X), parameters)
+                minibatch_total_loss = total_loss(logits, tf.transpose(minibatch_Y))
+                
+            train_accuracy.update_state(minibatch_Y, tf.transpose(logits))
+            trainable_variables = [W1, b1, W2, b2, W3, b3]
+            grads = tape.gradient(minibatch_total_loss, trainable_variables)
+            optimizer.apply_gradients(zip(grads, trainable_variables))
+            epoch_total_loss += minibatch_total_loss
+        epoch_total_loss /= m
+        
+        if print_cost == True and epoch % 10 == 0:
+            print ("Cost after epoch %i: %f" % (epoch, epoch_total_loss))
+            print("Train accuracy:", train_accuracy.result())
+            
+            for (minibatch_X, minibatch_Y) in test_minibatches:
+                Z3 = forward_propagation(tf.transpose(minibatch_X), parameters)
+                test_accuracy.update_state(minibatch_Y, tf.transpose(Z3))
+            print("Test_accuracy:", test_accuracy.result())
+
+            costs.append(epoch_total_loss)
+            train_acc.append(train_accuracy.result())
+            test_acc.append(test_accuracy.result())
+            test_accuracy.reset_state()
+
+    return parameters, costs, train_acc, test_acc
+
+parameters, costs, train_acc, test_acc = model(new_train, new_y_train, new_test, new_y_test, num_epochs=100)
+
+# plotting the cost function
+plt.plot(np.squeeze(costs))
+plt.ylabel('cost')
+plt.xlabel('iterations (per fives)')
+plt.show()
+
+# plotting the train accuracy
+plt.plot(np.squeeze(train_acc))
+plt.ylabel('Train Accuracy')
+plt.xlabel('iterations (per fives)')
+
+# plotting the test accuracy
+plt.plot(np.squeeze(test_acc))
+plt.ylabel('Test Accuracy')
+plt.xlabel('iterations (per fives)')
+plt.show()
