121f6f6293f124ab402b67cc207c05aa8580f01b
Nishchal Singh <nishchalsingh9a@gmail.com>
2025-05-24 18:09:42 +0530

    debug CNN

--- nn_multiClassification/threeLayer_multiClass.py
+++ nn_multiClassification/threeLayer_multiClass.py
@@ -4,7 +4,6 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import warnings
 warnings.filterwarnings("ignore")
 
-# import os
 # os.environ["CUDA_VISIBLE_DEVICES"] = "-1" 
 
 # importing required libraries
